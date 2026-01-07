5e3e04d679bd8d1b6fad270d244bc5e112bf6bb8
Xnths <jonathascastilhoes@gmail.com>
2026-01-07 16:31:23 -0300

    feat: testing if all nodes were visited

--- node-coverage/test.py
+++ node-coverage/test.py
@@ -3,22 +3,13 @@ sys.path.append("../")
 
 from graphs.cfg import ControlFlowGraph
 from util import get_construct, get_line, get_id
+from criteria.node_coverage import NodeCoverage
 
 source = open("./index.py").read()
-st = ControlFlowGraph(source)
+cfg = ControlFlowGraph(source)
+nc = NodeCoverage(cfg)
 
-for node in st.nodes():
+for node in cfg.nodes():
     print(get_construct(node), get_line(node), get_id(node))
 
-dfs = st.dfs()
-bfs = st.bfs()
-
-print("\ndfs")
-for (n1, n2) in dfs:
-    print(get_construct(n1), get_line(n1), get_id(n1))
-    print(get_construct(n2), get_line(n2), get_id(n2))
-
-print("\nbfs")
-for (n1, n2) in bfs:
-    print(get_construct(n1), get_line(n1), get_id(n1))
-    print(get_construct(n2), get_line(n2), get_id(n2))
+print(nc.test())
